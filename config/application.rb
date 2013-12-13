cd426f5d278e0a1e554768d028aecfffba2ca827
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2013-12-13 16:39:50 -0600

    Finish layout and routes

--- config/application.rb
+++ config/application.rb
@@ -66,5 +66,7 @@ module HartlTutorial
     config.assets.version = '1.0'
 
     config.assets.initialize_on_precompile = false
+
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
